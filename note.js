382a6826e509a247f7e849c37267d7fbf66170f2
Faisal ahmed <faisal2410@yahoo.com>
2023-09-01 18:44:08 +0600

    updated

--- note.js
+++ note.js
@@ -1,3 +1,5 @@
+// https://github.com/faisal2410/ecom_server_mern_b4
+
 // npm init --y
 // npm i nodemon -D
 // npm i bcrypt cors dotenv express express-formidable helmet jsonwebtoken mongoose morgan slugify
