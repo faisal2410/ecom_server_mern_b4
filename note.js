4a7d38011ba4bcdf803d7d7c95541a1a5a62552d
Faisal ahmed <faisal2410@yahoo.com>
2023-09-01 18:42:29 +0600

    creating server and folder structure

---
+++ note.js
@@ -0,0 +1,3 @@
+// npm init --y
+// npm i nodemon -D
+// npm i bcrypt cors dotenv express express-formidable helmet jsonwebtoken mongoose morgan slugify
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const helmet = require('helmet');
+
+require("dotenv").config();
+const morgan = require("morgan");
+const cors = require('cors');
+
+
+
+// middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(helmet())
+
+
+
+// server
+const port = process.env.PORT || 8000;
+
+
+
+app.listen(port, () => {
+    console.log(`Server Running on port ${port}`);
+});
+
