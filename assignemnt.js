c8f59e190670d6c3183b0662c1d3904b74acf7de
Faisal ahmed <faisal2410@yahoo.com>
2023-09-04 22:30:50 +0600

    updated

---
+++ assignemnt.js
@@ -0,0 +1,25 @@
+// Assignment :
+
+// In this assignment, you have to create a project structure with express.js. Create your folders and files with the following structure.
+
+// 1) Package.json is mandatory
+// 2) Index.js should be mandatory. where your application will run.
+// 3) App.js is mandatory. where the configuration will hold.
+// 4) Must have .gitignore . The file will contain '/node_modules'. So that your node_modules folder is not uploaded to GitHub.
+// 5) There should be a src folder. There will be five folders inside the folder Controllers, Helper, Middleware, Models, and Routes.
+// 6) There will be a total of ten Controller files inside the Controllers folder. In order they are - blogController.js, blogDetailsController.js, commentController.js, messageController.js, portfolioController.js, productController.js, profitController.js, projectController.js, serviceController.js, titleController.js
+// 7) Each controller will have four function exports. They are respectively - create, read, delete, and update.
+
+// For example : exports.create = async (req, res) => { };
+// exports.read = async (req, res) => { };
+// exports.delete = async (req, res) => { };
+// exports.update = async (req, res) => { };
+
+// 8) Each function of the controller should use res.send() method to pass some information. For example: res.send("Create Blog API")
+// 9) The Routes folder will contain the api.js file. And router path should be created in this file. Since there will be ten controller files and each file will have four functions, a total of 40 router paths will be created. Then you need to create 40 APIs. All APIs must be created through GET request.
+// 10) Helper, Middleware, and Models folders will be empty for now.
+
+
+
+// Submit Instruction:
+// Once the project structure is created, you upload the project to GitHub and submit the GitHub repository link.
