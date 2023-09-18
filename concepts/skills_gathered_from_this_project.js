cf04996bcce45ed97f3e85f8215c3456aaf08308
Faisal ahmed <faisal2410@yahoo.com>
2023-09-18 14:06:54 +0600

    updated

---
+++ concepts/skills_gathered_from_this_project.js
@@ -0,0 +1,150 @@
+// What you’ll learn from this Ecommerce project
+
+// 1. How to initialize your project with NPM
+// 2. How to create express server?
+// 3. How to continuously run express server using nodemon?
+// 4. How to use import export in NodeJs
+// 5. How to use ENV and gitignore files in NodeJs
+// 6. How to connect to Mongodb locally
+// 7. How to use mongo atlas as database in the cloud
+// 8. Routes and middlewares concept explained
+// 9. How to use controller functions
+// 10. How to create mongoose model using schema
+// 11. How to use bcrypt to hash and compare password
+// 12.How to use morgan as a middleware to log request endpoints
+// 13. How to use Postman to test Nodejs API
+// 14. How to save data in MongoDB
+// 15. How to register user with validation and error responses
+// 16. How to generate JWT( json web token)
+// 17. How to login user using compare password
+// 18. How to send authorization token in HTTP request headers
+// 19. How to protect routes using JWT verify
+// 20.How to protect routes for admin users
+// 21. How to create category model
+// 22. How to make a post request with JWT token in request headers and body
+// 23. How to create a category and save in MongoDB
+// 24. Creating category CRUD routes
+// 26. How to test CRUD API endpoints using Postman
+// 27. How to write product schema to create product model
+// 28. Creating product using JSON data vs form data
+// 29. How to handle form data with image upload and apply validation
+// 30. How to save a product in MongoDB
+// 31. How to send a list of products as JSON response
+// 32. How to fetch single product from MongoDB
+// 33. How to send photo for each product
+// 34. How to delete a product from MongoDB
+// 35. How to update product in MongoDB
+// 36. How to create a new react project
+// 37. How to create pages in React
+// 38. How to add routing system to react app
+// 39. How to use Bootstrap css in React app
+// 40. How to create a navigation menu using Bootstrap 5
+// 41. How to create a Jumbotron in Bootstrap 5
+// 42. How to make reusable components in React using props
+// 43. How to use useState hook in react to store user input
+// 44. How to create a user registration form in React
+// 45. Making API request using Axios
+// 46. How to inspect API request error message in React
+// 47. How to resolve CORS issue and show error messages using Toast Notifications
+// 48. How to create a working login page in React
+// 49. How to create context and access it’s value in React
+// 50. Saving user login response in context
+// 51. Preserve the state on page reload using local storage
+// 52. How to redirect user in React
+// 53. How to conditionally show and hide navigation links
+// 54. User dashboard page
+// 55. How to protect pages using private route in React using react router6
+// 56. How to delay the redirect using setinterval
+// 57. How to show loading indicator
+// 58. How to protect routes based on server response
+// 59. How to configure Axios with base url and token
+// 60. How to create 404 page not found in React
+// 61. How to take user to intended page after 
+// 62. How to create role based routing with dropdown option
+// 63. Create admin dashboard page
+// 64. How to restrict access to admin user only
+// 65. How to create admin sidebar menu
+// 66. Admin pages with shared sidebar menu
+// 67. User pages with shared sidebar
+// 68. How to make Axios POST request to create category from React app
+// 69. How to display all categories in React
+// 70. How to move code to components for reusability
+// 71. How to use Ant design Modal to update category
+// 72. How to make Axios PUT request to update category
+// 73. How to make Axios Delete request from React app
+// 74. How to use searchable Select dropdown to show categories list
+// 75. How to select image files to upload in React
+// 76. How to show image preview of uploading file in React
+// 77. How to build a form to create a product
+// 78. How to post form data to create product with image
+// 79. How to show list of products for admin
+// 80. How to render list of products in Bootstrap cards
+// 81. How to pre-populate product data in product update form
+// 82. How to make Axios PUT request to update product
+// 83. Fixing update product issues related to photo and shipping fields
+// 84. How to make Axios Delete request from react
+// 85. Error handling: cannot set headers after they are sent to client
+// 86. How to sort products based on created date and sold
+// 87. How to create 2 column responsive layout using Bootstrap
+// 88. How to use reusable product and component and display responsive image
+// 89. How to add buttons and hover effect to product card
+// 90. How to use ribbons to display product info in cards
+// 91. How to format product price based on local currency
+// 92. Jumbotron color animation css
+// 93. How to create animated background color for jumbotron
+// 94. Shop page with list of products
+// 95. How to show filter by categories option in checkbox
+// 96. How to show filter by price range option as radio buttons
+// 97. How to find products based on categories and price range filtering options
+// 98. How to render products based on filtering options in react
+// 99. How to add reset filtering options in react
+// 100. How to create paginated products endpoint in server
+// 101. How to add load more feature in React
+// 102. How to put search form in navigation menu
+// 103. How to create search component with state to make API request
+// 104. How to create MongoDB to get search results based on multiple properties
+// 105. How to replace local state with context in React
+// 106. How to display search results with search input in navigation menu
+// 107. How to create single product view page
+// 108. Display product in a single product view page
+// 109. How to use React icons
+// 110. Showing additional product information
+// 111. How to display related products in React ecommerce
+// 112. Display related products in product card
+// 113. How to create your own custom hook to fetch data
+// 114. How t show categories list in dropdown menu
+// 115. How to show list of categories on a page
+// 116. Create single category view page
+// 117. How to display products by category
+// 118. How to create cart context
+// 119. How to implement add to cart feature
+// 120. How to make sticky top menu.
+// 121. Create cart page
+// 122. How to display products in the cart page
+// 123. How to remove items from the cart
+// 124. Persist cart on page reload
+// 125. How to show cart total
+// 126. How to create a user profile update page in React
+// 127. How to implement user profile update including address
+// 128. Conditional rendering for delivery address
+// 129. Redirect user to cart page after login
+// 130. Signup to Braintree payment
+// 131. Braintree functions to generate token and process payment
+// 132. Show Braintree Web drop in UI in React
+// 133. Creating Paypal sandbox account
+// 134. Get payment method nonce on buy button click
+// 135. How to process payment with Braintree
+// 136. How to charge exact amount based on cart total
+// 137. Create a new order on successful payment
+// 138. How to add loading state to disable buy button
+// 139. How to get logged in user’s all orders
+// 140. Display order status in a table
+// 141. How to display user order’s each product
+// 142. How to decrement stock each time a product is sold
+// 143. Display list of orders for admin
+// 144. How to use Ant design’s Select component
+// 145. How to update order status by admin
+// 146. Sign in to sendgrid to get API keys for sending emails
+// 147. How to send email on order status change
+// 148. Receive order status update email
+// 149. Deployment to Vercel
