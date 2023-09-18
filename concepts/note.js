ddd4cc1dcfc46ccd9b63ec224fca01b7f2dfa90e
Faisal ahmed <faisal2410@yahoo.com>
2023-09-18 14:13:30 +0600

    updated

--- concepts/note.js
+++ concepts/note.js
@@ -1,5 +1,8 @@
 // https://github.com/faisal2410/ecom_server_mern_b4
 
+// Completed Backend
+// https://github.com/faisal2410/server
+
 // Skills we will gather from this project
 // https://atlantic-viola-f05.notion.site/Ecommerce-Project-14cd1cdaa9784d4f9a79711b84a43e01?pvs=4
 
